c384f8210305fd24679818c3be38fc0d59ef719c
anaiva27 <nastia.iv.91@gmail.com>
2024-07-29 09:48:48 -0700

    test copy

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,12 +1,14 @@
 import { Layout } from "Components/Root/Layout";
 import { LayoutTest } from "Components/Root/LayoutTest";
+import { LayoutTestCopy } from "Components/Root/LayoutTestCopy";
 import React from "react";
 import { Routes as Router, Route, useNavigate } from "react-router-dom";
 
 export const Routes = () => {
   return (
-    <Router basename="/GSAP-animation">
-      <Route path="/test" element={<LayoutTest />} />
+    <Router>
+      <Route path="/test" element={<LayoutTestCopy />} />
+      <Route path="/GSAP-animation/test" element={<LayoutTest />} />
       <Route path="/GSAP-animation/" element={<Layout />} />
     </Router>
   );
