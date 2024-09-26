f477ad951463b3670bc3bffd9daca88b53822e77
anaiva27 <nastia.iv.91@gmail.com>
2024-09-26 16:20:30 -0700

    hash attempt

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Routes } from "./Routes/Routes";
 import { Layout } from "./Components/Root/Layout";
 
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,14 +4,13 @@ import { Layout } from "Components/Root/Layout";
 import { LayoutTest } from "Components/Root/LayoutTest";
 import { LayoutTestCopy } from "Components/Root/LayoutTestCopy";
 import React from "react";
-import { Routes as Router, Route, useNavigate } from "react-router-dom";
+import { HashRouter as Router, Route, useNavigate } from "react-router-dom";
 
 export const Routes = () => {
   return (
     <Router basename="GSAP-animation">
       <Route path="/test" element={<LayoutTestCopy />} />
       <Route path="/ame" element={<AME />} />
-      <Route path="/test" element={<LayoutTest />} />
       <Route path="/GSAP-animation/test" element={<LayoutTest />} />
       <Route path="/GSAP-animation/" element={<Layout />} />
       <Route path="/audi/" element={<Audi />} />
