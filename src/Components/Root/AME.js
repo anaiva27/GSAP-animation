a06e544309a8b1c961664cc1eb8316542590f8ae
anaiva27 <nastia.iv.91@gmail.com>
2024-09-29 20:05:26 -0700

    AME layout update

--- src/Components/Root/AME.js
+++ src/Components/Root/AME.js
@@ -2,19 +2,19 @@ import React, { useContext, useState, useEffect, useRef } from "react";
 import styled, { css } from "styled-components";
 import bgImg from "Components/Assets/BG.png";
 import logo from "Components/Assets/Logo.png";
+import logoPattern from "Components/Assets/LogoPattern.png";
 import artist1 from "Components/Assets/artist1.png";
 import artist2 from "Components/Assets/artist2.png";
 import artist3 from "Components/Assets/artist3.png";
 import artist4 from "Components/Assets/artist4.png";
 import artist5 from "Components/Assets/artist5.png";
+import artist6 from "Components/Assets/artist6.png";
 import details from "Components/Assets/detailsImg.png";
+import City from "Components/Assets/City.png";
+import Calendar from "Components/Assets/Calendar.png";
+
 import logoMax from "Components/Assets/logoMax.png";
 import logoBrand from "Components/Assets/logoAudi.jpeg";
-import Lottie from "react-lottie";
-import * as animationData from "./count.json";
-import * as circleData from "./circlepulse01.json";
-import * as circleDataColor from "./pulse2.json";
-import * as testData from "./test3.json";
 import { useMediaQuery } from "../../Hooks";
 import "./styles.css";
 import { gsap } from "gsap";
@@ -24,25 +24,35 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export const AME = () => {
   gsap.registerPlugin(useGSAP, ScrollTrigger);
   const isMobile = useMediaQuery();
-
+  const [windowInnerWidth, setInnerWidth] = useState(window.innerWidth);
   const container = useRef(null);
   const containerCTA = useRef(null);
   const smoother = useRef();
+
+  useEffect(() => {
+    const _setInnerWidth = () => setInnerWidth(window.innerWidth);
+    _setInnerWidth();
+    window.addEventListener("resize", _setInnerWidth);
+    return () => {
+      window.removeEventListener("resize", _setInnerWidth);
+    };
+  }, []);
 
   useGSAP(
     () => {
       let tl = gsap.timeline();
       tl.from("#slider", { delay: 1, duration: 1, xPercent: -100 })
         .to("#logo", { opacity: 1, left: 0, right: 0 })
-        .to("#slider", { dalay: 3, duration: 1, xPercent: 100 })
+        .to("#slider", { duration: 1, xPercent: 100 })
         .to("#logo", {
           opacity: 1,
-          right: "-10%",
-          // top: "40%",
-          // position: "sticky",
+          right: "-100px",
         })
         .to("#content", { opacity: 1 })
-        .to(".tab", { stagger: 0.2, opacity: 1, y: 10 });
+        .to(".tab", { stagger: 0.2, opacity: 1, y: 10 })
+        .to("#logoPattern", {
+          opacity: 1,
+        });
     },
     { scope: container },
   );
@@ -51,20 +61,77 @@ export const AME = () => {
     <Wrapper ref={container} className="scroll">
       <Slider id="slider"></Slider>
       <Logo src={logo} alt="" id="logo" />
+      <LogoPattern src={logoPattern} alt="" id="logoPattern" />
       <Content as="section" id="content">
         <BrandContent>
           <MaxLogo src={logoMax} alt="" id="logo" />
           <BrandDetails>
-            <img src={logoBrand} alt="" id="logoBrand" />
-            <img src={details} alt="" id="details" />
+            <BrandInfo>
+              <H1>McDonald’s Lunar New Year Campaign</H1>
+              <LogoBox
+                hasSummary={true}
+                //isOval={shape === "oval"}
+              >
+                <BrandLogo
+                  src={logoBrand}
+                  //  isContain={alignment === "scale"}
+                />
+              </LogoBox>
+              <InfoContainer hasSummary={true}>
+                <div>
+                  <Icon src={City} />
+                  <Subtitle>McDonald's</Subtitle>
+                </div>
+                <TimeContainer>
+                  <Icon src={Calendar} />
+                  <Subtitle>Sep 04, 2024</Subtitle>
+                </TimeContainer>
+              </InfoContainer>
+              <h4>Summary</h4>
+              <Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in
+                est neque. In scelerisque lorem nec commodo accumsan. Sed semper
+                mauris fermentum massa facilisis, aliquam vehicula mauris
+                lobortis. Nulla porttitor accumsan dolor, quis vestibulum risus
+                iaculis vel. Aenean id placerat nisl
+              </Body>
+              {/* {!isMobile && <ListSummary summary={list?.introduction} />} */}
+            </BrandInfo>
           </BrandDetails>
+          <Highlight alignLeft={windowInnerWidth > 1500}>
+            <Subtitle>Sample Artist</Subtitle>
+          </Highlight>
+          <Caption>
+            NOTE: This Sample Artist List is provided for example purposes to
+            demonstrate the data available in the MAX Platform™ and the relative
+            size/type of artist we recommend... more
+          </Caption>
         </BrandContent>
         <ArtistListContent>
-          <img src={artist1} alt="" id="artist" className="tab" />
-          <img src={artist2} alt="" id="artist" className="tab" />
-          <img src={artist3} alt="" id="artist" className="tab" />
-          <img src={artist4} alt="" id="artist" className="tab" />
-          <img src={artist5} alt="" id="artist" className="tab" />
+          <div>
+            <div>
+              <img src={artist1} alt="" id="artist" className="tab" />
+            </div>
+            <div>
+              <img src={artist2} alt="" id="artist" className="tab" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src={artist3} alt="" id="artist" className="tab" />
+            </div>
+            <div>
+              <img src={artist4} alt="" id="artist" className="tab" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src={artist5} alt="" id="artist" className="tab" />
+            </div>
+            <div>
+              <img src={artist6} alt="" id="artist" className="tab" />
+            </div>
+          </div>
         </ArtistListContent>
       </Content>
     </Wrapper>
@@ -76,12 +143,11 @@ const Wrapper = styled.div`
   width: 100%;
   min-height: 100vh;
   position: relative;
-  background-color: #f7fafc;
+  background-color: #fff;
   overflow: hidden;
 `;
 const Logo = styled.img`
-  width: 20%;
-  object-fit: contain;
+  object-fit: cover;
   opacity: 0;
   max-width: 200px;
   position: absolute;
@@ -89,6 +155,18 @@ const Logo = styled.img`
   right: -10%;
   scale: 3;
   margin: auto 0 auto auto;
+  z-index: 1;
+`;
+const LogoPattern = styled.img`
+  object-fit: cover;
+  opacity: 0;
+  max-width: 300px;
+  position: absolute;
+  position: absolute;
+  top: 40%;
+  left: 150px;
+  scale: 2;
+  margin: auto auto auto 0;
   z-index: 1;
 `;
 const MaxLogo = styled.img`
@@ -108,41 +186,63 @@ const Slider = styled.div`
   left: 0;
 `;
 const Content = styled.div`
-  display: grid;
-  width: 85%;
-  grid-template-columns: 2fr 3fr;
+  display: flex;
+  /* width: 100%; */
+  margin: 0 auto;
+  justify-content: center;
   opacity: 0;
   z-index: 2;
+  position: relative;
   /* margin-right: 100px; */
   @media (max-width: 950px) {
-    grid-template-columns: 1fr;
+    flex-direction: column;
     margin: auto;
   }
 `;
 const BrandContent = styled.div`
   display: flex;
-  width: 100%;
+  width: 30%;
+  max-width: 500px;
   flex-direction: column;
   gap: 20px;
-  opacity: 1;
   z-index: 2;
-  left: 0;
-  top: 0;
   padding: 50px;
 `;
 const ArtistListContent = styled.div`
-  display: grid;
+  display: flex;
+  /* width: 100%; */
   flex-direction: column;
-  grid-template-columns: 1fr 1fr;
-  width: 100%;
-  opacity: 1;
-  padding-top: 50px;
+  gap: 15px;
+  padding: 140px 10vw 50px 0;
+  /* margin-right: auto; */
+  /* padding-right: 10vw; */
+  /* max-width: 1200px; */
+  /* flex: 2; */
   z-index: 2;
   max-height: 100vh;
-  overflow-y: scroll;
+  overflow-y: auto;
   img {
     width: 100%;
     //  max-width: 350px;
+  }
+  & > div {
+    /* max-width: 1000px; */
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    width: 100%;
+    div {
+      max-width: 500px;
+    }
+  }
+  scrollbar-width: thin;
+  scrollbar-color: transparent transparent;
+  & ::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+    opacity: 0;
+    /* position: absolute;
+    right: 0; */
   }
 `;
 const BrandDetails = styled.div`
@@ -151,17 +251,123 @@ const BrandDetails = styled.div`
   grid-template-columns: 1fr;
   opacity: 1;
   z-index: 2;
-  img {
+  & > img {
     width: 50%;
     max-width: 300px;
   }
+`;
+const BrandInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  h4 {
+    color: #000;
+  }
+  h1 {
+    margin-bottom: 10px;
+  }
+  h2 {
+    @media (max-width: 950px) {
+      font-size: 20px;
+      line-height: 28px;
+    }
+  }
+
+  span {
+    margin-top: 20px;
+    font-size: 15px;
+    color: #000;
+  }
+`;
+const Icon = styled.img`
+  border-radius: 25px;
+  width: 35px;
+  border: 1px solid #000;
+  padding: 3px;
+  margin-right: 12px;
+  @media (max-width: 950px) {
+    width: 30px;
+    padding: 1px;
+  }
+`;
+const TimeContainer = styled.div`
+  position: relative;
+  &:hover {
+    span {
+      visibility: visible;
+    }
+  }
+  @media (max-width: 950px) {
+    margin-top: 8px;
+  }
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 16px 0 10px 0;
+
+  ${(props) =>
+    props.hasSummary &&
+    css`
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 15px;
+    `}
+
+  @media (max-width: 950px) {
+    margin-top: 8px;
+    gap: 0px;
+    flex-direction: column;
+    align-items: flex-start;
+    p {
+      font-size: 13px;
+    }
+  }
+  div {
+    display: flex;
+    flex-direction: row;
+    margin-right: 40px;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const H1 = styled.h1`
+  color: black;
+  font-size: 30px;
+`;
+const Subtitle = styled.h2`
+  color: black;
+  font-weight: 500;
+  font-size: 20px;
+`;
+const Highlight = styled.div`
+  background-color: black;
+  width: calc(100% + 50px);
+  padding: 10px 10px 10px 50px;
+  transform: translateX(-50px);
+  h2 {
+    color: #fff;
+    border-left: 4px solid #fa017d;
+    padding-left: 10px;
+  }
+  ${({ alignLeft }) =>
+    alignLeft &&
+    css`
+      width: 100%;
+      padding: 10px 10px 10px 15px;
+      transform: translateX(0px);
+    `}
+`;
+const Caption = styled.p`
+  font-size: 12px;
+  color: #999999;
 `;
 const Body = styled.p`
   font-weight: 400;
   font-size: 14px;
   line-height: 150%;
   margin: 0;
-  color: #fff;
+  color: #000;
   ${({ registered }) => registered && "margin: 0 0 15px 0;"}
   a,
   a:active,
@@ -173,6 +379,48 @@ const Body = styled.p`
     `
   font-size: min(4vw, 18px);
   `}
+`;
+const LogoBox = styled.div`
+  display: flex;
+  background-color: #fff;
+  aspect-ratio: 1/1;
+  min-width: 200px;
+  max-width: 250px;
+  box-shadow: 0 0 20px 3px #e9e9e9;
+  /* border: 1px solid #e6ebea; */
+  margin-right: 50px;
+  overflow: hidden;
+
+  ${(props) =>
+    props.hasSummary &&
+    css`
+      min-width: 150px;
+      max-width: 150px;
+    `}
+
+  ${(props) =>
+    props.isOval &&
+    css`
+      border-radius: 50%;
+    `}
+  @media (max-width: 950px) {
+    min-width: unset;
+    min-height: unset;
+    width: 132px;
+    margin-right: 16px;
+  }
+`;
+const BrandLogo = styled.img`
+  width: 100%;
+  display: flex;
+  object-fit: cover;
+  object-position: center;
+  aspect-ratio: 1/1;
+  ${(props) =>
+    props.isContain &&
+    css`
+      object-fit: contain;
+    `}
 `;
 const Header = styled.div`
   width: 100%;
@@ -402,18 +650,18 @@ const SubHeading = styled.h3`
     white-space: nowrap;
   }
 `;
-const HighLight = styled.h3`
-  font-size: min(2vw, 20px);
-  font-weight: 500;
-  color: #e71e3d;
-  span {
-    white-space: nowrap;
-  }
-  img {
-    position: relative;
-    top: 5px;
-  }
-`;
+// const HighLight = styled.h3`
+//   font-size: min(2vw, 20px);
+//   font-weight: 500;
+//   color: #e71e3d;
+//   span {
+//     white-space: nowrap;
+//   }
+//   img {
+//     position: relative;
+//     top: 5px;
+//   }
+// `;
 
 // ======== ARTIST IMAGE / RIGHT SIDE ======== //
 
